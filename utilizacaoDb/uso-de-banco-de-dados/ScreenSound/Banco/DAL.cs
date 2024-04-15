836400eb8b3f6cc555e55ee52731866fd8558177
Matheus <matheus22lima2005@gmail.com>
2024-04-15 15:48:26 -0300

    ''

---
+++ utilizacaoDb/uso-de-banco-de-dados/ScreenSound/Banco/DAL.cs
@@ -0,0 +1,75 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class DAL<T> where T : class
+    {
+        protected readonly ScreenSoundContext context;
+        public DAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+        public  IEnumerable<T> Listar()
+        {
+            try
+            {
+                return context.Set<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Enumerable.Empty<T>();
+            }
+        }
+        public void Adicionar(T objeto)
+        {
+            try
+            {
+                context.Set<T>().Add(objeto);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Atualizar(T objeto)
+        {
+            try
+            {
+                context.Set<T>().Update(objeto);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Deletar(T objeto)
+        {
+            try
+            {
+                context.Set<T>().Remove(objeto);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public T? ProcurarPor(Func<T, bool> condicao)
+        {
+            return context.Set<T>().FirstOrDefault(condicao);
+        }
+        public IEnumerable<T> ListarPor(Func<T, bool> condicao)
+        {
+            return context.Set<T>().Where(condicao);
+        }
+
+    }
+}
---
+++ utilizacaoDb/uso-de-banco-de-dados/ScreenSound/Menus/MenuRegistrarMusica.cs
@@ -0,0 +1,35 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarMusica : Menu
+{
+    public override void Executar(DAL<Artista> artistaDal)
+    {
+        base.Executar(artistaDal);
+        ExibirTituloDaOpcao("Registro de músicas");
+        Console.Write("Digite o artista cuja música deseja registrar: ");
+        string nomeDoArtista = Console.ReadLine()!;
+        var recuperarArtista = artistaDal.ProcurarPor(a => a.Nome.Equals(nomeDoArtista));
+        if (recuperarArtista is not null)
+        {
+            Console.Write("Agora digite o título da música: ");
+            string tituloDaMusica = Console.ReadLine()!;
+            Console.Write("Agora digite o ano de lançamento da música: ");
+            string anoLancamento = Console.ReadLine()!;
+            recuperarArtista.AdicionarMusica(new Musica(tituloDaMusica) { AnoLancamento = Convert.ToInt32(anoLancamento)});
+            artistaDal.Atualizar(recuperarArtista);
+            Console.WriteLine($"A música {tituloDaMusica} de {nomeDoArtista} foi registrada com sucesso!");
+            Thread.Sleep(4000);
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"\nO artista {nomeDoArtista} não foi encontrado!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
